99babe6bf090bc52442c4f4e9c3ca7e071c0cc58
김도영 <31909322+kimdy003@users.noreply.github.com>
2022-01-30 17:36:14 +0900

    Time: 248 ms (48.08%), Space: 23.1 MB (12.60%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
@@ -8,50 +8,49 @@ from collections import deque
 
 
 class Codec:
-
     def serialize(self, root):
         """Encodes a tree to a single string.
-        
+
         :type root: TreeNode
         :rtype: str
         """
         if not root:
-            return ''
-        
+            return ""
+
         queue = deque([root])
         res = []
-        
+
         while queue:
             node = queue.popleft()
-            
+
             if node:
                 res.append(str(node.val))
                 queue.append(node.left)
                 queue.append(node.right)
             else:
-                res.append('null')
-        return ' '.join(res)
-        
+                res.append("null")
+        return " ".join(res)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
-        
+
         :type data: str
         :rtype: TreeNode
         """
         if len(data) == 0:
             return []
-        
+
         data = data.split()
         for i, val in enumerate(data):
             data[i] = TreeNode(val)
-            
-        for i in range(1, len(data)+1):
+
+        for i in range(1, len(data) + 1):
             if i * 2 <= len(data):
-                data[i -1].left = data[(i*2) - 1]
+                data[i - 1].left = data[(i * 2) - 1]
             if i * 2 + 1 <= len(data):
-                data[i - 1].right = data[(i*2+1) - 1]
+                data[i - 1].right = data[(i * 2 + 1) - 1]
         return data[0]
+
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
