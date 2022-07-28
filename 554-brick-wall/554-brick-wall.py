3153d7cd98ff81a3a577c14dd75c532a05a073c7
김도영 <31909322+kimdy003@users.noreply.github.com>
2022-07-28 19:48:17 +0900

    Time: 277 ms (54.15%), Space: 19.4 MB (5.64%) - LeetHub

---
+++ 554-brick-wall/554-brick-wall.py
@@ -0,0 +1,13 @@
+import collections
+
+
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        d = collections.defaultdict(int)
+        for line in wall:
+            i = 0
+            for brick in line[:-1]:
+                i += brick
+                d[i] += 1
+        print(len(wall), d)
+        return len(wall) - max(d.values(), default=0)
