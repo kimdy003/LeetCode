be2558f5f4090c1d60b084a87989a56b0364ab6b
김도영 <31909322+kimdy003@users.noreply.github.com>
2022-10-02 18:12:46 +0900

    Time: 98 ms (95.11%), Space: 14 MB (34.70%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -2,32 +2,31 @@ from typing import List
 
 
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        ans = []
+    def isValidSudoku(self, board):
+        return (self.is_row_valid(board) and
+                self.is_col_valid(board) and
+                self.is_square_valid(board))
 
-        def checkFunction(arr):
-            for b in arr:
-                check = [0] * 10
-                for num in b:
-                    if num != ".":
-                        n = int(num)
-                        check[n] += 1
-                        if check[n] > 1:
-                            return False
-            return True
+    def is_row_valid(self, board):
+        for row in board:
+            if not self.is_unit_valid(row):
+                return False
+        return True
 
-        ans.append(checkFunction(board))
-        ans.append(checkFunction(list(map(list, zip(*board)))))
+    def is_col_valid(self, board):
+        for col in zip(*board):
+            if not self.is_unit_valid(col):
+                return False
+        return True
 
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                check = [0] * 10
-                for y in range(i, i + 3):
-                    for x in range(j, j + 3):
-                        if board[y][x] != ".":
-                            n = int(board[y][x])
-                            check[n] += 1
-                            if check[n] > 1:
-                                return False
-        else:
-            return all(ans)
+    def is_square_valid(self, board):
+        for i in (0, 3, 6):
+            for j in (0, 3, 6):
+                square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                if not self.is_unit_valid(square):
+                    return False
+        return True
+
+    def is_unit_valid(self, unit):
+        unit = [i for i in unit if i != '.']
+        return len(set(unit)) == len(unit)
